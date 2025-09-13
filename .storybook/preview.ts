2c5954dc9218ff33209a37b412e55ebdaf6a0fa2
l-chess <liltiz1209@gmail.com>
2025-09-13 20:22:48 +0200

    cleaned up stylesheet

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,12 @@
-import type { Preview } from '@storybook/nextjs'
-import "/home/letizia/OnlyBooksFrontend/src/app/globals.css"
+import type { Preview } from "@storybook/nextjs";
+import "/home/letizia/OnlyBooksFrontend/src/app/globals.css";
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
