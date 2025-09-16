aef41dcc05a7c64a1ccf9cc70e370264b226b256
l-chess <liltiz1209@gmail.com>
2025-09-16 16:02:52 +0200

    created ChatOptions

--- src/components/molecules/ImageUpload/ImageUpload.stories.tsx
+++ src/components/molecules/ImageUpload/ImageUpload.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { ImageUpload } from "./ImageUpload";
 
 const meta = {
-  title: "Atoms/ImageUpload",
+  title: "Molecules/ImageUpload",
   component: ImageUpload,
   tags: ["autodocs"],
 } satisfies Meta<typeof ImageUpload>;
