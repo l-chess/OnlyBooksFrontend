a6eb0b765bc6bc58475e95c07974270c7a15d2e4
l-chess <liltiz1209@gmail.com>
2025-09-17 13:01:25 +0200

    created chat teaser

---
+++ src/components/molecules/ChatTeaser/ChatTeaser.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { ChatTeaser } from "./ChatTeaser";
+
+const meta = {
+  title: "Molecules/ChatTeaser",
+  component: ChatTeaser,
+  tags: ["autodocs"],
+} satisfies Meta<typeof ChatTeaser>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    cover: {
+      src: "https://booklounge.co.za/wp-content/uploads/2022/08/the-song-of-achilles-1.jpg",
+      alt: "The Song of Achilles",
+      className: "",
+    },
+    title: "The Song of Achilles",
+    latestMessage: "Ja, ist noch zu haben.",
+    timeStamp: "1h",
+  },
+};
